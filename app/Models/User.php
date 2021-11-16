659b8fdaf3a51a3e87daf8f262d55e75e63c4fce
davitlabadze <labadzedato18@gmail.com>
2021-11-16 23:11:01 +0400

    feat: gravatar image with test

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,21 @@ class User extends Authenticatable
     {
         return $this->hasMany(Idea::class);
     }
+
+    public function getAvatar()
+    {
+        $firstCharacter = $this->email[0];
+
+        $integerToUse =is_numeric($firstCharacter)
+        ? ord(strtolower($firstCharacter)) - 21
+        : $integerToUse = ord(strtolower($firstCharacter)) - 96;
+
+
+        return 'https://www.gravatar.com/avatar/'
+            .md5($this->email)
+            .'?s=200'
+            .'&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+            .$integerToUse
+            .'.png';
+    }
 }
