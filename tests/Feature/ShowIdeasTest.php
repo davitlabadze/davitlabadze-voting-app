9c12eac0573f72ba7fbbed893a2b7d5226093de5
davitlabadze <labadzedato18@gmail.com>
2021-11-17 12:30:09 +0400

    test: creat idea

--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -86,11 +86,11 @@ class ShowIdeasTest extends TestCase
         $ideaEleven->title = 'My Eleventh Idea';
         $ideaEleven->save();
         $response = $this->get('/');
-        $response->assertSee($ideaOne->title);
-        $response->assertDontSee($ideaEleven->title);
-        $response = $this->get('/?page=2');
         $response->assertSee($ideaEleven->title);
         $response->assertDontSee($ideaOne->title);
+        $response = $this->get('/?page=2');
+        $response->assertSee($ideaOne->title);
+        $response->assertDontSee($ideaEleven->title);
     }
     /** @test */
     public function same_idea_title_different_slugs()
