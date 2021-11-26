4f2baa2fbef986d696d221bfaa25ab950ef8add6
davitlabadze <labadzedato18@gmail.com>
2021-11-26 21:29:47 +0400

    test: edit idea

--- app/Http/Livewire/EditIdea.php
+++ app/Http/Livewire/EditIdea.php
@@ -29,7 +29,6 @@ class EditIdea extends Component
 
     public function updateIdea()
     {
-        // Authorization
         if (auth()->guest() || auth()->user()->cannot('update', $this->idea)) {
             abort(Response::HTTP_FORBIDDEN);
         }
